343dd1d53540b95c6283d1cd13f7f824a7fd795d
Marcin Kumorek <kumeek@gmail.com>
2020-02-27 19:54:02 +0100

    cleanup :speak_no_evil:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>
+          Study<span>Deck</span>
+        </h1>
+        <h2>Study, sleep, repeat...</h2>
       </header>
+      <main>
+        <h1>Main</h1>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

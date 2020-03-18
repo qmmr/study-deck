c688908ef6e0476fde9ce5b1fb4a54461533e09f
Marcin Kumorek <kumeek@gmail.com>
2020-03-18 22:20:43 +0100

    Add proxy for API calls :+1:

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,9 +1,9 @@
 export async function getCards() {
-  const data = await fetch(`https://carpal-cliff-dianella.glitch.me/api/card`)
+  const data = await fetch(`/api/card`)
   return await data.json()
 }
 
 // type TRemoveCard = (id: string) => Promise<T>
 export async function removeCard(id: string) {
-  return await fetch(`https://carpal-cliff-dianella.glitch.me/api/card/${id}`, { method: 'DELETE' })
+  return await fetch(`/api/card/${id}`, { method: 'DELETE' })
 }

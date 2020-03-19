48de20e671b112ec6fd5b56d8a1592d3b93c812b
Marcin Kumorek <kumeek@gmail.com>
2020-03-19 19:25:39 +0100

    Use rebass instad styled-components :sweat_smile:

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,122 @@
 import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
+import { Flex } from 'rebass'
 import './App.css'
+
+import { ThemeProvider } from 'emotion-theming'
 
 import { getCards } from './services/cardService'
 import CardView from './components/CardView'
+import CardForm from './components/CardForm'
 
-const AppContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const CardsContainer = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-evenly;
-`
+// default theme preset
+export const preset = {
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#30c',
+    muted: '#f6f6f9',
+    gray: '#dddddf',
+    highlight: 'hsla(205, 100%, 40%, 0.125)',
+  },
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'inherit',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25,
+  },
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  sizes: {
+    avatar: 48,
+  },
+  radii: {
+    default: 4,
+    circle: 99999,
+  },
+  shadows: {
+    card: '0 0 4px rgba(0, 0, 0, .125)',
+  },
+  // rebass variants
+  text: {
+    heading: {
+      fontFamily: 'heading',
+      lineHeight: 'heading',
+      fontWeight: 'heading',
+    },
+    display: {
+      fontFamily: 'heading',
+      fontWeight: 'heading',
+      lineHeight: 'heading',
+      fontSize: [5, 6, 7],
+    },
+    caps: {
+      textTransform: 'uppercase',
+      letterSpacing: '0.1em',
+    },
+  },
+  variants: {
+    avatar: {
+      width: 'avatar',
+      height: 'avatar',
+      borderRadius: 'circle',
+    },
+    card: {
+      p: 2,
+      bg: 'background',
+      boxShadow: 'card',
+    },
+    link: {
+      color: 'primary',
+    },
+    nav: {
+      fontSize: 1,
+      fontWeight: 'bold',
+      display: 'inline-block',
+      p: 2,
+      color: 'inherit',
+      textDecoration: 'none',
+      ':hover,:focus,.active': {
+        color: 'primary',
+      },
+    },
+  },
+  buttons: {
+    primary: {
+      fontSize: 2,
+      fontWeight: 'bold',
+      color: 'background',
+      bg: 'primary',
+      borderRadius: 'default',
+    },
+    outline: {
+      variant: 'buttons.primary',
+      color: 'primary',
+      bg: 'transparent',
+      boxShadow: 'inset 0 0 2px',
+    },
+    secondary: {
+      variant: 'buttons.primary',
+      color: 'background',
+      bg: 'secondary',
+    },
+  },
+  styles: {
+    root: {
+      fontFamily: 'body',
+      fontWeight: 'body',
+      lineHeight: 'body',
+    },
+  },
+}
 
 export type TCard = {
   id: string
@@ -25,11 +127,11 @@ export type TCard = {
 export type TCards = Array<TCard>
 
 function App() {
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState<TCards>([])
 
   useEffect(() => {
     async function fetchData() {
-      const json = await getCards()
+      const json: TCards = await getCards()
       // console.log('json data: ', json)
       setCards(json)
     }
@@ -40,23 +142,30 @@ function App() {
     setCards(cards.filter((card: TCard): boolean => card.id !== id))
   }
 
+  const handleCardAdd = (card: TCard): void => {
+    setCards(existingCards => [...existingCards, card])
+  }
+
   return (
-    <AppContainer>
-      <header>
-        <h1>
-          Study<span>Deck</span>
-        </h1>
-        <h2>Study, sleep, repeat...</h2>
-      </header>
-      <main>
-        <h1>Main</h1>
-        <CardsContainer>
-          {cards.map((card: TCard) => (
-            <CardView key={card.id} onRemove={handleCardRemove} {...card} />
-          ))}
-        </CardsContainer>
-      </main>
-    </AppContainer>
+    <ThemeProvider theme={preset}>
+      <Flex flexDirection="column">
+        <header>
+          <h1>
+            Study<span>Deck</span>
+          </h1>
+          <h2>Study, sleep, repeat...</h2>
+        </header>
+        <main>
+          <h1>Main</h1>
+          <CardForm onSave={handleCardAdd} />
+          <Flex flexDirection="row" flexWrap="wrap" justifyContent="space-evenly">
+            {cards.map((card: TCard) => (
+              <CardView key={card.id} onRemove={handleCardRemove} {...card} />
+            ))}
+          </Flex>
+        </main>
+      </Flex>
+    </ThemeProvider>
   )
 }
 

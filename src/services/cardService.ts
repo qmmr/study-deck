bd89faee26734c9040645405dad4a86cddbcb36d
Marcin Kumorek <kumeek@gmail.com>
2020-03-19 19:26:41 +0100

    Update cardService :muscle:

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,9 +1,23 @@
-export async function getCards() {
-  const data = await fetch(`/api/card`)
-  return await data.json()
+export async function getCards<T>(): Promise<T> {
+  const response = await fetch(`/api/card`)
+
+  return await response.json()
 }
 
-// type TRemoveCard = (id: string) => Promise<T>
-export async function removeCard(id: string) {
-  return await fetch(`/api/card/${id}`, { method: 'DELETE' })
+// TODO: Convert to arrow function syntax
+export async function removeCard<T>(id: string): Promise<T> {
+  const response = await fetch(`/api/card/${id}`, { method: 'DELETE' })
+
+  return response.json()
+}
+
+// TODO: Convert to arrow function syntax
+export async function saveCard<T>(card: { term: string; definition: string }): Promise<T> {
+  const response = await fetch(`/api/card`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(card),
+  })
+
+  return response.json()
 }
